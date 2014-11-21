74f69f7c493fda1406b55598c080e38fea3084d1
ocReaper <ocreaper@gmail.com>
2014-11-21 15:33:53 +0100

    add maplander repo the to project

---
+++ js/vendor/maplander/markers.js
@@ -0,0 +1,64 @@
+var datass = {
+    net: [{
+            name: "Evie Wise",
+            age: 26,
+            pin: "js/vendor/maplander/images/pin1.png",
+            avatar: "js/vendor/maplander/images/avatar1.png"
+        }, {
+            name: "Kayla Rossiter",
+            age: 23,
+            pin: "js/vendor/maplander/images/pin2.png",
+            avatar: "js/vendor/maplander/images/avatar2.png"
+        }, {
+            name: "Madeleine Hammer",
+            age: 25,
+            pin: "js/vendor/maplander/images/pin3.png",
+            avatar: "js/vendor/maplander/images/avatar3.png"
+        }, {
+            name: "Alana Muramats",
+            age: 24,
+            pin: "js/vendor/maplander/images/pin4.png",
+            avatar: "js/vendor/maplander/images/avatar4.png"
+        }, {
+            name: "Kayla Barclay",
+            age: 27,
+            pin: "js/vendor/maplander/images/pin5.png",
+            avatar: "js/vendor/maplander/images/avatar5.png"
+        }, {
+            name: "Caitlin Stawell",
+            age: 26,
+            pin: "js/vendor/maplander/images/pin6.png",
+            avatar: "js/vendor/maplander/images/avatar6.png"
+        }, {
+            name: "Lauren Monson",
+            age: 25,
+            pin: "js/vendor/maplander/images/pin7.png",
+            avatar: "js/vendor/maplander/images/avatar7.png"
+        }, {
+            name: "Stella Doolette",
+            age: 22,
+            pin: "js/vendor/maplander/images/pin8.png",
+            avatar: "js/vendor/maplander/images/avatar8.png"
+        }, {
+            name: "Sophie Lindell",
+            age: 21,
+            pin: "js/vendor/maplander/images/pin9.png",
+            avatar: "js/vendor/maplander/images/avatar9.png"
+        }, {
+            name: "Alicia Tout",
+            age: 25,
+            pin: "js/vendor/maplander/images/pin10.png",
+            avatar: "js/vendor/maplander/images/avatar10.png"
+        }, {
+            name: "Stephanie Knorr",
+            age: 26,
+            pin: "js/vendor/maplander/images/pin11.png",
+            avatar: "js/vendor/maplander/images/avatar11.png"
+        }, {
+            name: "Lauren Flierl",
+            age: 24,
+            pin: "js/vendor/maplander/images/pin12.png",
+            avatar: "js/vendor/maplander/images/avatar12.png"
+        }
+    ]
+};
